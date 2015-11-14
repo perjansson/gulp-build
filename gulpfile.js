d27fe347c96f8617553a69ea819cc7f940317789
Per Jansson <per.r.jansson@gmail.com>
2015-11-14 20:38:58 +0100

    concat, uglify, minify-css

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,33 @@
 var gulp = require('gulp'),
   connect = require('gulp-connect'),
   bower = require('main-bower-files'),
-  inject = require('gulp-inject');
+  inject = require('gulp-inject'),
+  rename = require('gulp-rename'),
+  concat = require('gulp-concat'),
+  uglify = require('gulp-uglify'),
+  minifyCSS = require('gulp-minify-css');
 
 var scripts = 'app/scripts/**/*.js';
 var styles = 'app/styles/**/*.css';
+var dist = './dist/';
+var dist_scripts = 'scripts.min.js';
+var dist_styles = 'styles.min.css';
 
-gulp.task('build', function() {
+gulp.task('scripts', function() {
+  return gulp.src(scripts)
+    .pipe(uglify())
+    .pipe(concat(dist_scripts))
+    .pipe(gulp.dest(dist));
+});
+
+gulp.task('styles', function() {
+  return gulp.src(styles)
+    .pipe(minifyCSS())
+    .pipe(concat(dist_styles))
+    .pipe(gulp.dest(dist));
+});
+
+gulp.task('build', ['scripts', 'styles'], function() {
   return gulp.src('index.html')
     .pipe(inject(
       gulp.src(bower(), {
@@ -16,13 +37,13 @@ gulp.task('build', function() {
         relative: true
       }))
     .pipe(inject(
-      gulp.src(scripts, {
+      gulp.src(dist + dist_scripts, {
         read: false
       }), {
         relative: true
       }))
     .pipe(inject(
-      gulp.src(styles, {
+      gulp.src(dist + dist_styles, {
         read: false
       }), {
         relative: true
